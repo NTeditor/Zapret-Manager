99b371aaaddd4b548b5a8a6d0485933338b03a82
NTeditor <nteditor4@gmail.com>
2025-03-27 15:18:38 +0300

    init

---
+++ app/src/main/java/ru/nteditor/zapretkotlin/MainActivity.kt
@@ -0,0 +1,96 @@
+package ru.nteditor.zapretkotlin
+
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.io.File
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    private fun List<String>.runCommand(workingDir: File): String? {
+        try {
+            val proc = ProcessBuilder(this)
+                .directory(workingDir)
+                .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                .redirectError(ProcessBuilder.Redirect.PIPE)
+                .start()
+
+            proc.waitFor(60, TimeUnit.MINUTES)
+            return proc.inputStream.bufferedReader().readText()
+        } catch(e: IOException) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val textTester1 = findViewById<TextView>(R.id.textTester1)
+        val zapretButtonStop = findViewById<Button>(R.id.zapretButtonStop)
+        val zapretButtonStart = findViewById<Button>(R.id.zapretButtonStart)
+        val zapretStatusCard = findViewById<TextView>(R.id.zapretStatusCard)
+
+        fun zapretCheckStatus() {
+            var zapretStatusCMD =
+                listOf("su", "-c", "pidof", "nfqws").runCommand(File("/system/bin"))
+            if (zapretStatusCMD == "") {
+                var zapretStatus = "запрет не работает"
+                zapretStatusCard.text = zapretStatus
+
+            } else if (zapretStatusCMD == "/system/bin/sh: su: inaccessible or not found") {
+                var zapretStatus = "нет рута $zapretStatusCMD"
+                zapretStatusCard.text = zapretStatus
+            } else {
+                var zapretStatus = "запрет работает"
+                zapretStatusCard.text = zapretStatus
+            }
+
+        }
+
+        zapretCheckStatus()
+
+        zapretButtonStart.setOnClickListener {
+
+            val outputCMD = listOf("su", "-c", "zapret", "start").runCommand(File("/system"))
+            textTester1.text = outputCMD
+            zapretCheckStatus()
+
+
+
+
+        }
+
+        zapretButtonStop.setOnClickListener {
+            val outputCMD = listOf("su", "-c", "zapret", "stop").runCommand(File("/system"))
+            textTester1.text = outputCMD
+            zapretCheckStatus()
+
+
+        }
+
+
+    }
+}
