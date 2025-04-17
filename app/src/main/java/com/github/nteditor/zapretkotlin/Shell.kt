0118f6e7126d35190ea0c7147ae26ac15ea9a139
NTeditor <nteditor4@gmail.com>
2025-04-17 22:24:29 +0300

    Добавленны классы Shell и IsShell из dev ветки

---
+++ app/src/main/java/com/github/nteditor/zapretkotlin/Shell.kt
@@ -0,0 +1,37 @@
+package com.github.nteditor.zapretkotlin
+
+import java.io.File
+import java.io.IOException
+import java.lang.IllegalArgumentException
+
+
+class Shell(private val command: List<String>) {
+
+
+    private fun List<String>.runCommand(): String {
+        try {
+            val proc = ProcessBuilder(this)
+                .directory(File("/"))
+                .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                .redirectError(ProcessBuilder.Redirect.PIPE)
+                .start()
+
+            return proc.inputStream.bufferedReader().readText()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return "Error"
+        }
+    }
+
+    private fun getCommand(): List<String> {
+        if (command == listOf("")) {
+            throw IllegalArgumentException("Command list is empty")
+        }
+        return command
+    }
+
+    fun start(): String {
+        return getCommand().runCommand()
+    }
+
+}
